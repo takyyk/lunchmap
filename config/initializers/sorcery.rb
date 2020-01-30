cca5fd3b83ac6f6116e65f4dc85b678e4846f4b5
takaykuki0310 <t.k.y079c@gmail.com>
2020-01-30 19:55:25 +0900

    introduce: sorcery

---
+++ config/initializers/sorcery.rb
@@ -0,0 +1,19 @@
+# The first thing you need to configure is which modules you need in your app.
+# The default is nothing which will include only core features (password encryption, login/logout).
+#
+# Available submodules are: :user_activation, :http_basic_auth, :remember_me,
+# :reset_password, :session_timeout, :brute_force_protection, :activity_logging,
+# :magic_login, :external
+Rails.application.config.sorcery.submodules = []
+
+# Here you can configure each submodule's features.
+Rails.application.config.sorcery.configure do |config|
+  # --- user config ---
+  config.user_config do |user|
+    user.stretches = 1 if Rails.env.test?
+  end
+
+  # This line must come after the 'user config' block.
+  # Define which model authenticates with sorcery.
+  config.user_class = 'User'
+end
