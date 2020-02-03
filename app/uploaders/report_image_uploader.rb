2f57665ecfb3117239799a6dd972993ff8444e26
takaykuki0310 <t.k.y079c@gmail.com>
2020-02-03 14:46:06 +0900

    fix: error

--- app/uploaders/report_image_uploader.rb
+++ app/uploaders/report_image_uploader.rb
@@ -22,11 +22,6 @@ class ReportImageUploader < CarrierWave::Uploader::Base
     1..5.megabytes
   end
 
-  # 保存するファイルの命名規則
-  def filename
-    "#{secure_token}.#{file.extension}" if original_filename.present?
- end
-
   def default_url
     'sample.jpg'
   end
