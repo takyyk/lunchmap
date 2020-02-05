b6a050c6efdc668027e22a40ed96d60b8a11ae00
takaykuki0310 <t.k.y079c@gmail.com>
2020-02-05 20:16:55 +0900

    add: report title uniqueness

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,5 @@
 class Report < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 255 }
+  validates :title, presence: true, length: { maximum: 255 }, uniqueness: true
   validates :body, presence: true, length: { maximum: 65_535 }
 
   belongs_to :user
