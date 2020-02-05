f3bde2d5b326e8fec5ac5fa749e62a20285533cd
takaykuki0310 <t.k.y079c@gmail.com>
2020-02-05 20:18:37 +0900

    update: add model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#full_name" do
+    let(:user) { FactoryBot.build(:user) }
+
+    it "returns first and last name" do
+      result = user.decorate.full_name
+
+      expect(result).to eq "#{user.last_name} #{user.first_name}"
+    end
+  end
 end
