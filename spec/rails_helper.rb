3ba0b7f131fb0bc53bfdee2b89cc68c3da3a0658
takaykuki0310 <t.k.y079c@gmail.com>
2020-02-06 10:04:52 +0900

    update: add: config.include FactoryBot::Syntax::Methods

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,5 +30,6 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
 
+  config.include FactoryBot::Syntax::Methods
   config.include LoginMacros
 end
